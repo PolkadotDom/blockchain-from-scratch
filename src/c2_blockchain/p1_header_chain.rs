d211b4b447e806a6e79a4ff3b80e2f5a217cf7f8
Polka.Dom <polkadotdom@gmail.com>
2024-01-04 14:23:07 -0500

    C2_P1

--- src/c2_blockchain/p1_header_chain.rs
+++ src/c2_blockchain/p1_header_chain.rs
@@ -24,12 +24,24 @@ pub struct Header {
 impl Header {
 	/// Returns a new valid genesis header.
 	fn genesis() -> Self {
-		todo!("Exercise 1")
+		Header {
+			parent: u64::MIN, 
+			height: u64::MIN,
+			extrinsics_root: (),
+			state_root: (),
+			consensus_digest: (),
+		}
 	}
 
 	/// Create and return a valid child header.
 	fn child(&self) -> Self {
-		todo!("Exercise 2")
+		Header {
+			parent: hash(self), 
+			height: self.height + 1,
+			extrinsics_root: (),
+			state_root: (),
+			consensus_digest: (),
+		}
 	}
 
 	/// Verify that all the given headers form a valid chain from this header to the tip.
@@ -37,22 +49,35 @@ impl Header {
 	/// This method may assume that the block on which it is called is valid, but it
 	/// must verify all of the blocks in the slice;
 	fn verify_sub_chain(&self, chain: &[Header]) -> bool {
-		todo!("Exercise 3")
+		let mut parent = self;
+		for header in chain {
+			if hash(parent) != header.parent || header.height != parent.height + 1 {
+				return false;
+			}
+			parent = header;
+		}
+		return true;
 	}
 }
 
-// And finally a few functions to use the code we just
+// And finally a few functions to use the code we just wrote
 
 /// Build and return a valid chain with exactly five blocks including the genesis block.
 fn build_valid_chain_length_5() -> Vec<Header> {
-	todo!("Exercise 4")
+	let mut headers = vec![Header::genesis(); 5];
+	for i in 1..5 {
+		headers[i] = headers[i-1].child();
+	}
+	headers
 }
 
 /// Build and return a chain with at least three headers.
 /// The chain should start with a proper genesis header,
 /// but the entire chain should NOT be valid.
 fn build_an_invalid_chain() -> Vec<Header> {
-	todo!("Exercise 5")
+	let mut headers = build_valid_chain_length_5();
+	headers.push(headers[1].child());
+	headers
 }
 
 // To run these tests: `cargo test bc_1
