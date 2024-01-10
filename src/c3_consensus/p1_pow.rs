3663bf47b86862f338a2c9651df88b6cb46c4f83
Polka.Dom <polkadotdom@gmail.com>
2024-01-10 11:38:05 -0500

    C3_P1

--- src/c3_consensus/p1_pow.rs
+++ src/c3_consensus/p1_pow.rs
@@ -4,7 +4,10 @@
 //! This is the same logic we implemented previously. Here we re-implement it in the
 //! generic consensus framework that we will use throughout the rest of the chapter.
 
+use std::num::ParseIntError;
+
 use super::{Consensus, Header};
+use crate::hash;
 
 /// A Proof of Work consensus engine. This is the same consensus logic that we
 /// implemented in the previous chapter. Here we simply re-implement it in the
@@ -19,13 +22,19 @@ impl Consensus for PoW {
 	/// Check that the provided header's hash is below the required threshold.
 	/// This does not rely on the parent digest at all.
 	fn validate(&self, _: &Self::Digest, header: &Header<Self::Digest>) -> bool {
-		todo!("Exercise 1")
+		header.consensus_digest < self.threshold
 	}
 
 	/// Mine a new PoW seal for the partial header provided.
 	/// This does not rely on the parent digest at all.
 	fn seal(&self, _: &Self::Digest, partial_header: Header<()>) -> Option<Header<Self::Digest>> {
-		todo!("Exercise 2")
+		let mut header: Header<Self::Digest> = partial_header.convert_to_digest(u64::MIN);
+		let mut hashed = hash(&header);
+		while hashed >= self.threshold {
+			header.consensus_digest += 1;
+			hashed = hash(&header);
+		}
+		Some(header)
 	}
 }
 
@@ -33,5 +42,7 @@ impl Consensus for PoW {
 /// with randomly drawn nonces will be valid. That is: the threshold should be u64::max_value() /
 /// 100.
 pub fn moderate_difficulty_pow() -> impl Consensus {
-	todo!("Exercise 3")
+	PoW {
+		threshold: u64::max_value() / 100
+	}
 }
