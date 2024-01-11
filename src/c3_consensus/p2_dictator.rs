1e85c99fc2e6ec457524d97dba73044225726482
Polka.Dom <polkadotdom@gmail.com>
2024-01-10 20:15:08 -0500

    C3_P2

--- src/c3_consensus/p2_dictator.rs
+++ src/c3_consensus/p2_dictator.rs
@@ -20,11 +20,11 @@ impl Consensus for DictatorConsensus {
 
 	/// Check that the header is signed by the dictator
 	fn validate(&self, _: &Self::Digest, header: &Header<Self::Digest>) -> bool {
-		todo!("Exercise 1")
+		header.consensus_digest == self.dictator
 	}
 
 	/// Sign the given partial header by the dictator
 	fn seal(&self, _: &Self::Digest, partial_header: Header<()>) -> Option<Header<Self::Digest>> {
-		todo!("Exercise 2")
+		Some(partial_header.convert_to_digest(self.dictator))
 	}
 }
